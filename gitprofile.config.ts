6e71e9df0b183203f1a95fde4a1baba12e0ab5d7
Julián Rodríguez <74310216+San1estus@users.noreply.github.com>
2026-05-11 13:39:39 -0600

    Change default theme from 'dark' to 'dim'

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -122,7 +122,7 @@ const CONFIG = {
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: { id: '', snippetVersion: 6 },
   themeConfig: {
-    defaultTheme: 'dark',
+    defaultTheme: 'dim',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
